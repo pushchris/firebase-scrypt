87e6ab8b624279be0691830817084cd36bab101a
Chris Anderson <hi@chrisanderson.io>
2019-06-20 19:19:01 -0500

    Re-add pacapt fetch

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo '  [x] Clone pacapt repo'
-sudo wget https://github.com/icy/pacapt/raw/ng/pacapt
+sudo wget -O /usr/local/bin/pacapt https://github.com/icy/pacapt/raw/ng/pacapt
+sudo chmod 755 /usr/local/bin/pacapt
+sudo ln -sv /usr/local/bin/pacapt /usr/local/bin/pacman || true
 
 echo '  [x] Install SLL dev dependency'
 sudo pacapt install libssl-dev 
