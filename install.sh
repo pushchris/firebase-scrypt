694bdcb48c0d8fb58dd5aaf88a8d3b3da8c7dd80
Chris Anderson <hi@chrisanderson.io>
2019-06-20 19:06:06 -0500

    Temp remove pacapt

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
-echo '  [x] Clone pacapt repo'
-sudo wget -O /usr/local/bin/pacapt \
-https://github.com/icy/pacapt/raw/ng/pacapt
-sudo chmod 755 /usr/local/bin/pacapt
-sudo ln -sv /usr/local/bin/pacapt /usr/local/bin/pacman || true
+
 
 echo '  [x] Install SLL dev dependency'
 sudo pacapt install libssl-dev 
@@ -11,7 +7,7 @@ echo '  [x] Install automake'
 sudo pacapt install automake
 
 echo '  [x] Clone firebase Scrypt repo'
-sudo git clone https://github.com/firebase/scrypt.git
+git clone https://github.com/firebase/scrypt.git
 
 echo '  [x] Build Scrypt'
 cd scrypt/
